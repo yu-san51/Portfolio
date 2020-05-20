edf125f08ce311b66324c11885a3519ef35c142d
yu-san51 <nekonyan.yuki51@gmail.com>
2020-05-20 08:51:06 +0000

    [Add] contractmodel

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_20_082347) do
+ActiveRecord::Schema.define(version: 2020_05_20_083423) do
+
+  create_table "contracts", force: :cascade do |t|
+    t.integer "contractor", null: false
+    t.integer "contractee", null: false
+    t.integer "item_id", null: false
+    t.integer "is_status", default: 0, null: false
+    t.integer "contract_price", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "favorites", force: :cascade do |t|
     t.integer "uesr_id", null: false
