06a0226a65d7c6560a23645294216de590ab3806
yu-san51 <nekonyan.yuki51@gmail.com>
2020-05-22 09:50:50 +0000

    [Add] relationship

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,12 @@
 class Item < ApplicationRecord
 
+	belongs_to :user
+	has_many :favorites, dependent: :destroy
+	has_many :contracts, dependent: :destroy
+	has_many :rooms, dependent: :destroy
+	has_many :item_style, dependent: :destroy
+	has_many :style, througt: :item_style, dependent: :destroy
+
 	enum prefer_sex: {どちらでも可: 0, 男性: 1, 女性: 2}
 	enum is_continue: {継続案件: true, 単発案件: false}
 	enum is_active: {受付中: true, 受付停止中: false}
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,2 +1,5 @@
 class Room < ApplicationRecord
+
+	belongs_to :item
+	has_many :messages, dependent: :destroy
 end
