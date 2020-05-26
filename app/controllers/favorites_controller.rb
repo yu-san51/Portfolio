c67dde7585c380dc4d8edb8b99b8c7a79bc121dc
yu-san51 <nekonyan.yuki51@gmail.com>
2020-05-26 07:40:31 +0000

    [Add] favorite

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,7 +1,15 @@
 class FavoritesController < ApplicationController
+
   def create
+  	@item = Item.find(params[:item_id])
+  	favorite = current_user.favorites.new(item_id: @item.id)
+  	favorite.save
   end
 
   def destroy
+
+  	@item = Item.find(params[:item_id])
+  	favorite = current_user.favorites.find_by(item_id: @item.id)
+  	favorite.destroy
   end
 end
