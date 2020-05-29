8bb018fd6a70ab55eeedc1ca04a4c406f78111ae
yu-san51 <nekonyan.yuki51@gmail.com>
2020-05-29 00:08:41 +0000

    [Fix] except message_creat

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,19 +3,25 @@ class MessagesController < ApplicationController
 	def show
 		@message = Message.new
 		@room = Room.find(params[:id])
-		@messages = Message.all
+		@messages = @room.messages.all
+
+			# if @room.item.user_id == current_user.id
+			# 	@receiver = @room.item.user_id
+			# else
+			# 	@receiver = current_user.id
+			# end
+		#end
+		# messages is nil, message receiver ni @room.item.user_id wo ireru
+		# messages is not nil, @room.item.user_id == current_user.id
 	end
-	
+
 	def create
-		@message = Message.new(message_params)
-		@message.receiver = User.find(params[:receiver])
-		@message.sender = current_user.id
-		@message.room_id = @room.id
+		@message = current_user.messages.new(message_params)
 		@message.save
 	end
 
 	private
 	def message_params
-		params.require(:message).permit(:content, :sender, :reciever, :room_id)
+		params.require(:message).permit(:content, :sender, :receiver, :room_id)
 	end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -2,4 +2,6 @@ class Message < ApplicationRecord
 
 	belongs_to :sender, class_name: "User"
 	belongs_to :receiver, class_name: "User"
+	# belongs_to :sender_id, class_name: "User"
+	# belongs_to :receiver_id, class_name: "User"
 end
