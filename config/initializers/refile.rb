1403363dde648040932764895c0908a9c33ba062
yu-san51 <nekonyan.yuki51@gmail.com>
2020-06-21 09:04:13 +0000

    [Fix] envfile

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -1,10 +1,5 @@
 require "refile/s3"
 
-aws = {
-  access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-  secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-  region: "ap-northeast-1",
-  bucket: "pf-infra",
-}
-Refile.cache = Refile::S3.new(prefix: "cache", **aws)
-Refile.store = Refile::S3.new(prefix: "store", **aws)
+
+Refile.cache = Refile::S3.new(prefix: "cache")
+Refile.store = Refile::S3.new(prefix: "store")
